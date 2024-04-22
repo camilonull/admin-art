10e746e4cfa3284e097118c120e4cb0bec0a7ba2
camilonull <camilo.vargas10@uptc.edu.co>
2024-04-22 17:42:59 -0500

    Update items, need service conect and show list ok or delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SuggestionComponent } from './pages/suggestion/suggestion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path:'login',component:LoginComponent},
+  {path:'suggestion',component:SuggestionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SuggestionComponent } from './pages/suggestion/suggestion.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SuggestionComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
